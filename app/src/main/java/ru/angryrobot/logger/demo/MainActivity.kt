37ffb032b1a9f654cd176f49b3fc0ec99a40f980
Alexander <alexander@angryrobot.ru>
2022-05-09 17:42:16 +0300

    updated ui for demo app

--- app/src/main/java/ru/angryrobot/logger/demo/MainActivity.kt
+++ app/src/main/java/ru/angryrobot/logger/demo/MainActivity.kt
@@ -1,8 +1,13 @@
 package ru.angryrobot.logger.demo
 
 
+import android.R
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
+import ru.angryrobot.logger.LogLevel
 import ru.angryrobot.logger.Logger
 import ru.angryrobot.logger.demo.databinding.ActivityMainBinding
 import java.io.File
@@ -26,11 +31,21 @@ class MainActivity : AppCompatActivity() {
 
         }
 
+        binding.logLevel.apply {
+            adapter = ArrayAdapter(this@MainActivity , R.layout.simple_spinner_dropdown_item, R.id.text1, LogLevel.values())
+            onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long ) {
+                    log.logLevel = LogLevel.values()[position]
+                }
+                override fun onNothingSelected(parent: AdapterView<*>?) {  }
+            }
+        }
+
         binding.logVerbose.setOnClickListener {
             log.v("Simple \'verbose\' log entry")
         }
         binding.logDebug.setOnClickListener {
-            log.v("\'Debug\' log entry with custom tag", tag = "SomeTag")
+            log.d("\'Debug\' log entry with custom tag", tag = "SomeTag")
         }
         binding.logInfo.setOnClickListener {
             log.i("Info")
--- logger/src/main/java/ru/angryrobot/logger/Logger.kt
+++ logger/src/main/java/ru/angryrobot/logger/Logger.kt
@@ -3,7 +3,6 @@ package ru.angryrobot.logger
 /***
  * Добавить принтТехникалДата и всякие интересные штуки для логирования
  * Добавить логирование номера линии
- * dateFormat - вынести в конфиг
  * Посмотреть как там в джаве оно будет работать
  */
 import android.annotation.SuppressLint
@@ -12,6 +11,8 @@ import android.os.SystemClock
 import android.util.Log
 import androidx.annotation.IntRange
 import java.io.*
+import java.lang.IllegalStateException
+import java.text.DateFormat
 import java.text.SimpleDateFormat
 import java.util.*
 import java.util.logging.FileHandler
@@ -20,10 +21,14 @@ import java.util.logging.LogRecord
 import java.util.zip.ZipEntry
 import java.util.zip.ZipOutputStream
 
-class Logger(logsDir: File?) {
+class Logger {
 
     var writeToLogcat = true
     var writeToFile = true
+        set(value) {
+            if (value && settings == null) throw IllegalStateException("Logger is not configured to write logs to the files")
+            field = value
+        }
 
     var logLevel: LogLevel = LogLevel.VERBOSE
 
@@ -32,39 +37,35 @@ class Logger(logsDir: File?) {
 
     private val fileHandler: FileHandler?
 
-    @SuppressLint("ConstantLocale")
-    private val dateFormat = SimpleDateFormat("MM-dd HH:mm:ss.SSS", Locale.US)
     private val pid = Process.myPid().toString()
 
+    val settings: LoggerSettings?
 
-    val settings: LoggerSettings
+    constructor() : this(null)
 
-    init {
-        settings = LoggerSettings(logsDir)
-        fileHandler = if (logsDir != null) {
+    constructor(logsDir: File) : this(LoggerSettings(logsDir))
 
-            if (!logsDir.exists()) logsDir.mkdirs()
-
+    constructor(settings: LoggerSettings?) {
+        this.settings = settings
+        fileHandler = if (settings != null) {
+            if (!settings.logsDir.exists()) settings.logsDir.mkdirs()
             FileHandler(
-                File(logsDir.absolutePath, settings.fileSettings.logFilesName).absolutePath,
-                settings.fileSettings.logFilesSize,
-                settings.fileSettings.logFilesCount,
-                true
+                File(settings.logsDir.absolutePath, settings.logFilesName).absolutePath,
+                settings.logFilesSize, settings.logFilesCount, true
             ).apply {
                 formatter = object : java.util.logging.Formatter() {
                     override fun format(record: LogRecord) = record.message
                 }
             }
-
         } else {
+            writeToFile = false
             null
         }
     }
 
-
     fun createLogBundle(zipFile: File) {
         //TODO надо проверить зачем нам это вообще?
-        val files = settings.fileSettings.logsDir?.listFiles() ?: throw IOException("Can't create zip file. No logs yet")
+        val files = settings?.logsDir?.listFiles() ?: throw IOException("Can't create zip file. No logs yet")
         val zipOutput = ZipOutputStream(FileOutputStream(zipFile))
         for (file in files) {
             if (file.name.endsWith(".lck")) continue
@@ -78,9 +79,9 @@ class Logger(logsDir: File?) {
     }
 
     private fun writeToFile(priority: LogLevel, tag: String, message: String) {
-        if (fileHandler != null) {
-            val string = "${dateFormat.format(Date())} ${priority.string}/$tag ($pid): $message\n"
-            fileHandler.publish(LogRecord(Level.INFO, string))
+        if (settings != null) {
+            val string = "${settings.timeFormat.format(Date())} ${priority.string}/$tag ($pid): $message\n"
+            fileHandler?.publish(LogRecord(Level.INFO, string))
         }
     }
 
@@ -134,7 +135,10 @@ class Logger(logsDir: File?) {
             "[${element.className.split(".").last()}.${element.methodName}]"
         }
 
-        if (writeToLogcat) Log.println(logLevel.code, tag, message.toString())
+        if (writeToLogcat) {
+            Log.println(logLevel.code, tag, message.toString())
+            exception?.printStackTrace()
+        }
 
         if (logEvent) {
             firebaseLogger?.invoke("${logLevel.string}/$tag $message")
@@ -146,8 +150,6 @@ class Logger(logsDir: File?) {
         if (writeToFile) {
             writeToFile(logLevel, tag, message.toString())
             if (exception != null) {
-                // TODO проверку добавить,  может не хотим писать ничего сюда
-                exception.printStackTrace()
                 val exceptionString = getStackTrace(exception)
                 writeToFile(logLevel, tag, exceptionString)
             }
@@ -168,37 +170,37 @@ class Logger(logsDir: File?) {
     }
 }
 
-class LoggerSettings(logsDir: File?) {
+class LoggerSettings(
 
-    var writeToLogcat = true
-    var writeToFile = logsDir != null
-    val fileSettings: FileSettings = FileSettings(logsDir)
+    /**
+     * The directory where the logs will be stored. If the field is `null`, no logs are written to the file
+     */
+    val logsDir: File,
 
-    class FileSettings(
-        /**
-         * The directory where the logs will be stored. If the field is `null`, no logs are written to the file
-         */
-        val logsDir: File?,
+    /**
+     * The maximum number of bytes to write to any one file. When the limit is reached - a new file will be created
+     * If the field is zero, the file size is unlimited
+     */
+    @IntRange(from = 0) val logFilesSize: Int = 1024 * 1024,
 
-        /**
-         * The maximum number of bytes to write to any one file. When the limit is reached - a new file will be created
-         * If the field is zero, the file size is unlimited
-         */
-        @IntRange(from = 0) val logFilesSize: Int = 1024 * 1024,
+    /**
+     * Maximum number of log files in the directory
+     */
+    @IntRange(from = 1) val logFilesCount: Int = 5,
 
-        /**
-         * Maximum number of log files in the directory
-         */
-        @IntRange(from = 1) val logFilesCount: Int = 5,
+    /**
+     * Sets the name for the log files. The logger will add a number to the end of the name,
+     * so the files in the `logsDir` directory will have names likes "logfile.txt.0", "logfile.txt.1" etc...
+     */
+    val logFilesName: String = "logfile.txt",
 
-        /**
-         * Sets the name for the log files. The logger will add a number to the end of the name,
-         * so the files in the `logsDir` directory will have names likes "logfile.txt.0", "logfile.txt.1" etc...
-         */
-        val logFilesName: String = "logfile.txt"
-    )
+    /**
+     * Timestamp format
+     */
+    @SuppressLint("ConstantLocale")
+    val timeFormat: DateFormat = SimpleDateFormat("MM-dd HH:mm:ss.SSS", Locale.US)
 
-}
+)
 
 enum class LogLevel(val code: Int, val string: String) {
     VERBOSE(Log.VERBOSE, "V"),
